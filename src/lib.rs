8791b52d0ac2666f20b3b618dda98b59baeb70c9
Roland Mikhel <Roolli9705@gmail.com>
2021-01-15 19:00:20 +0100

    configured different build targets

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,19 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
-#[cfg(feature = "wee_alloc")]
+
+#[cfg(all(target_arch = "wasm32", feature = "wee_alloc"))]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, horizon!");
+fn main() {
+    env_logger::init();
 }

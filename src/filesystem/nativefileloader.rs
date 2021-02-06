130e91178da50bf22981ebc6ce95088bad268adb
Roland Mikhel <Roolli9705@gmail.com>
2021-02-06 03:10:00 +0100

    changed path storage to owned

--- src/filesystem/nativefileloader.rs
+++ src/filesystem/nativefileloader.rs
@@ -1,22 +1,26 @@
 use super::fileloader::FileLoader;
 use async_trait::async_trait;
-use std::{fs::File, io::Read, path::Path};
-pub struct Nativefileloader<P: AsRef<Path>> {
-    rootdir: P,
+use std::{
+    fs::File,
+    io::Read,
+    path::{Path, PathBuf},
+};
+pub struct Nativefileloader {
+    rootdir: PathBuf,
 }
 
-impl<P: AsRef<Path>> Nativefileloader<P> {
-    pub fn new(rootdir: P) -> Self {
+impl Nativefileloader {
+    pub fn new(rootdir: PathBuf) -> Self {
         Self { rootdir }
     }
 }
 
 #[async_trait(?Send)]
-impl<T: AsRef<Path>> FileLoader for Nativefileloader<T> {
+impl FileLoader for Nativefileloader {
     // Might change to using async_std if this isn't fast enough.
     async fn load_file(&self, path: &str) -> Vec<u8> {
         let mut vec = Vec::new();
-        let file_path = self.rootdir.as_ref().join(path);
+        let file_path = self.rootdir.clone().join(path);
         let mut f = File::open(file_path).unwrap();
         f.read_to_end(&mut vec).unwrap();
         vec

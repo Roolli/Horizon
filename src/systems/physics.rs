bc912f605a3b62fe747cacd7cc704301c4927c18
Roland Mikhel <Roolli9705@gmail.com>
2021-02-28 00:40:06 +0100

    event handling skeleton for physics events

--- src/systems/physics.rs
+++ src/systems/physics.rs
@@ -1,10 +1,24 @@
-use std::ops::Deref;
+use std::{
+    borrow::BorrowMut,
+    cell::{Cell, RefCell},
+    ops::Deref,
+    time::Instant,
+};
 
 use glm::Vec3;
 use rapier3d::{
-    dynamics::{IntegrationParameters, JointSet, RigidBody, RigidBodyHandle, RigidBodySet},
-    geometry::{BroadPhase, Collider, ColliderHandle, ColliderSet, NarrowPhase},
-    pipeline::PhysicsPipeline,
+    crossbeam::{
+        self,
+        channel::{Receiver, Sender},
+    },
+    dynamics::{
+        BodyStatus, IntegrationParameters, JointSet, RigidBody, RigidBodyHandle, RigidBodySet,
+    },
+    geometry::{
+        BroadPhase, Collider, ColliderHandle, ColliderSet, ContactEvent, IntersectionEvent,
+        NarrowPhase,
+    },
+    pipeline::{ChannelEventCollector, PhysicsPipeline},
 };
 use specs::{Join, Read, ReadExpect, ReadStorage, System, WriteExpect, WriteStorage};
 
@@ -21,26 +35,42 @@ pub struct PhysicsWorld {
     body_set: RigidBodySet,
     collider_set: ColliderSet,
     joints: JointSet,
-    event_handler: (),
+    event_handler: ChannelEventCollector,
     gravity: Vec3,
+    contact_event_receiver: Receiver<ContactEvent>,
+    intersection_event_receiver: Receiver<IntersectionEvent>,
 }
 
 impl PhysicsWorld {
     pub fn new(gravity_vector: Vec3) -> Self {
+        let (contact_send, contact_receive) = crossbeam::channel::unbounded();
+        let (intersection_send, intersection_receive) = crossbeam::channel::unbounded();
+        let event_handler = ChannelEventCollector::new(intersection_send, contact_send);
         Self {
             pipeline: PhysicsPipeline::new(),
             body_set: RigidBodySet::new(),
             broad_phase: BroadPhase::new(),
             narrow_phase: NarrowPhase::new(),
             collider_set: ColliderSet::new(),
-            event_handler: (),
+            event_handler,
+            contact_event_receiver: contact_receive,
+            intersection_event_receiver: intersection_receive,
             gravity: gravity_vector,
-            integration_parameters: IntegrationParameters::default(),
+            integration_parameters: IntegrationParameters {
+                ..Default::default()
+            },
             joints: JointSet::new(),
         }
     }
+
+    pub fn get_contact_receiver(&self) -> &Receiver<ContactEvent> {
+        &self.contact_event_receiver
+    }
+    pub fn get_intersection_receiver(&self) -> &Receiver<IntersectionEvent> {
+        &self.intersection_event_receiver
+    }
     pub fn add_rigid_body(&mut self, rb: RigidBody) -> RigidBodyHandle {
-        self.body_set.insert(rb)
+        self.body_set.borrow_mut().insert(rb)
     }
     pub fn add_collider(
         &mut self,

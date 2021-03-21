bc52ee31bb60668f87986069f43ac09772ccd0a3
Roland Mikhel <Roolli9705@gmail.com>
2021-03-21 01:07:53 +0100

    optimized directional light bound calculation

--- src/renderer/light.rs
+++ src/renderer/light.rs
@@ -3,6 +3,8 @@ use bytemuck::*;
 use glm::{look_at_rh, ortho_rh};
 use wgpu::BindGroup;
 
+use crate::resources::camera::Camera;
+
 use super::{
     model::HorizonModel,
     primitives::{instance, mesh::Mesh, uniforms::Globals},
@@ -15,59 +17,92 @@ pub struct LightHandle {
     pub index: usize,
 }
 
-pub struct Light {
-    pub pos: glm::Vec3,
+pub struct DirectionalLight {
+    pub direction: glm::Vec3,
     color: wgpu::Color,
-    fov: f32,
-    pub depth: Range<f32>,
-    pub target_view: wgpu::TextureView,
 }
 
-impl Light {
-    pub fn new(
-        pos: glm::Vec3,
-        color: wgpu::Color,
-        fov: f32,
-        depth: Range<f32>,
-        target_view: wgpu::TextureView,
-    ) -> Self {
-        Self {
-            pos,
-            color,
-            fov,
-            depth,
-            target_view,
-        }
+impl DirectionalLight {
+    pub fn new(direction: glm::Vec3, color: wgpu::Color) -> Self {
+        Self { direction, color }
     }
 
-    pub fn to_raw(&self) -> LightRaw {
-        let proj =
-        //(-10.0, 10., -10., -10., self.depth.start, self.depth.end);
-        //glm::perspective(1.0, f32::to_radians(self.fov), self.depth.start, self.depth.end);
-        glm::ortho_rh(-80.0, 80.0, -80.0, 80.0, self.depth.start, self.depth.end);
-        let view = glm::look_at_rh(&self.pos, &glm::vec3(0.0, 0.0, 0.0), &glm::Vec3::y());
+    pub fn to_raw(&self, cam: &Camera) -> DirectionalLightRaw {
+        let view = glm::look_at_rh(&glm::vec3(0.0, 0.0, 0.0), &self.direction, &glm::Vec3::y());
+        let proj = Self::get_frustum_bounding_box(&self, cam, cam.z_near, cam.z_far, &view);
         let view_proj = glm::Mat4::identity()
             * glm::Mat4::from(super::state::State::OPENGL_TO_WGPU_MATRIX)
             * proj
             * view;
-        LightRaw {
+        DirectionalLightRaw {
+            direction: [self.direction.x, self.direction.y, self.direction.z, 1.0],
             color: [
                 self.color.r as f32,
                 self.color.g as f32,
                 self.color.b as f32,
                 1.0,
             ],
-            position: [self.pos.x, self.pos.y, self.pos.z, 1.0],
             projection: view_proj.into(),
         }
+    }
+    fn get_frustum_bounding_box(
+        &self,
+        cam: &Camera,
+        znear: f32,
+        zfar: f32,
+        light_space: &glm::Mat4,
+    ) -> glm::Mat4 {
+        let cam_view = glm::look_at_rh(&cam.eye, &cam.target, &cam.up);
+        let cam_inverse = cam_view.try_inverse().unwrap();
+
+        let tan_half_horizontal_fov = f32::tan(f32::to_radians(cam.fov_y / 2.0));
+        let tan_half_vertical_fov = f32::tan(f32::to_radians((cam.fov_y * cam.aspect_ratio) / 2.0));
+        let x_near = znear * tan_half_horizontal_fov;
+        let x_far = zfar * tan_half_horizontal_fov;
+
+        let y_near = znear * tan_half_vertical_fov;
+        let y_far = zfar * tan_half_vertical_fov;
+        let frustum_corners = [
+            // near face
+            glm::Vec4::new(x_near, y_near, znear, 1.0),
+            glm::Vec4::new(-x_near, y_near, znear, 1.0),
+            glm::Vec4::new(x_near, -y_near, znear, 1.0),
+            glm::Vec4::new(-x_near, -y_near, znear, 1.0),
+            // far face
+            glm::Vec4::new(x_far, y_far, zfar, 1.0),
+            glm::Vec4::new(-x_far, y_far, zfar, 1.0),
+            glm::Vec4::new(x_far, -y_far, zfar, 1.0),
+            glm::Vec4::new(-x_far, -y_far, zfar, 1.0),
+        ];
+        let frustum_corners_l: Vec<glm::Vec4> = Vec::new();
+
+        let min_x = f32::MAX;
+        let min_y = f32::MAX;
+        let min_z = f32::MAX;
+        let max_x = f32::MIN;
+        let max_y = f32::MIN;
+        let max_z = f32::MIN;
+        for i in 0..8 {
+            // world space
+            let vw = cam_inverse * frustum_corners[i];
+            frustum_corners_l[i] = light_space * vw;
+            min_x = f32::min(min_x, frustum_corners_l[i].x);
+            min_y = f32::min(min_y, frustum_corners_l[i].y);
+            min_z = f32::min(min_z, frustum_corners_l[i].z);
+
+            max_x = f32::max(max_x, frustum_corners_l[i].x);
+            max_y = f32::max(max_y, frustum_corners_l[i].y);
+            max_z = f32::max(max_z, frustum_corners_l[i].z);
+        }
+        glm::ortho_rh(min_x, max_x, min_y, max_y, min_z, max_z)
     }
 }
 
 #[repr(C)]
 #[derive(Clone, Copy, Zeroable, Pod)]
-pub struct LightRaw {
+pub struct DirectionalLightRaw {
     pub projection: [[f32; 4]; 4],
-    pub position: [f32; 4],
+    pub direction: [f32; 4],
     pub color: [f32; 4],
 }
 

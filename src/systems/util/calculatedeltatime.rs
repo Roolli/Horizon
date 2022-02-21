376aab7d2aa66c05fd45606c7fa64ff03b2d90ef
Roland Mikhel <roolli9705@gmail.com>
2022-02-21 16:41:30 +0100

    update debug ui to show fps

--- src/systems/util/calculatedeltatime.rs
+++ src/systems/util/calculatedeltatime.rs
@@ -2,13 +2,15 @@ use std::ops::Add;
 use chrono::{Duration, Timelike};
 use specs::{System, Write, WriteExpect};
 use crate::resources::deltatime::DeltaTime;
+use crate::ui::debugstats::DebugStats;
 
 pub struct UpdateDeltaTime;
 
 impl<'a> System<'a> for UpdateDeltaTime {
-    type SystemData = Write<'a,DeltaTime>;
+    type SystemData = (Write<'a,DeltaTime>,WriteExpect<'a,DebugStats>);
 
-    fn run(&mut self, mut delta_time: Self::SystemData) {
+
+    fn run(&mut self, (mut delta_time,mut debug_ui): Self::SystemData) {
 
         let now = chrono::offset::Utc::now();
         let delta = (now - delta_time.previous_frame_time).timestamp_nanos();
@@ -19,7 +21,7 @@ impl<'a> System<'a> for UpdateDeltaTime {
             delta_time.frame_count += 1;
         } else {
             delta_time.prev_sec_frame_count = delta_time.frame_count;
-            log::info!(target:"Frame Count","FPS: {}", delta_time.frame_count);
+            debug_ui.fps = delta_time.prev_sec_frame_count;
             delta_time.frame_count = 0;
             delta_time.total_frame_time = Duration::seconds(0);
         }

9da3a63f4bf6d758f5c64623ffff91b9d4e31a02
Roland Mikhel <roolli9705@gmail.com>
2022-02-21 16:42:04 +0100

    add component types enum to facilitate scripting

--- src/components.rs
+++ src/components.rs
@@ -1,4 +1,5 @@
 pub mod assetidentifier;
+pub mod componenttypes;
 pub mod modelcollider;
 pub mod physicshandle;
 pub mod scriptingcallback;
---
+++ src/components/componenttypes.rs
@@ -0,0 +1,8 @@
+use wasm_bindgen::prelude::*;
+#[wasm_bindgen]
+pub enum ComponentTypes {
+    AssetIdentifier,
+    ModelCollider,
+    PhysicsHandle,
+    Transform,
+}

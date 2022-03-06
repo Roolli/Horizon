90b0553130a80e8e3d0da2358c41b127eaec266e
Roland Mikhel <Roolli9705@gmail.com>
2022-03-06 02:02:30 +0100

    modify collider creation for static objects (still needs some work with min bounds calc)

--- src/components.rs
+++ src/components.rs
@@ -5,13 +5,3 @@ pub mod modelcollider;
 pub mod physicshandle;
 pub mod scriptingcallback;
 pub mod transform;
-
-pub struct ComponentStorage<T> {
-    inner: T,
-}
-
-impl<T> ComponentStorage<T> {
-    pub fn new(inner: T) -> Self {
-        ComponentStorage { inner }
-    }
-}

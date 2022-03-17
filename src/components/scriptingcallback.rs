67d7ea92692e4d46636532b3b780a789a47aedb9
Roland Mikhel <Roolli9705@gmail.com>
2022-03-18 00:20:14 +0100

    add uniform way of calling callbacks

--- src/components/scriptingcallback.rs
+++ src/components/scriptingcallback.rs
@@ -2,6 +2,8 @@ use specs::*;
 
 
 #[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+#[cfg(target_arch = "wasm32")]
 #[derive(Component)]
 #[storage(VecStorage)]
 pub struct ScriptingCallback {
@@ -18,6 +20,16 @@ impl ScriptingCallback {
         &self.callback
     }
 }
+#[cfg(target_arch = "wasm32")]
+impl ExecuteFunction for ScriptingCallback {
+    fn execute_with_no_args(&self) {
+        self.get_callback().call0(&JsValue::UNDEFINED).unwrap();
+    }
+
+    fn execute_with_args(&self,args: Vec<f32>) {
+        self.get_callback().call1(&JsValue::NULL,&JsValue::from(args[0])).unwrap();
+    }
+}
 
 
 #[cfg(not(target_arch = "wasm32"))]
@@ -25,4 +37,24 @@ impl ScriptingCallback {
 #[storage(VecStorage)]
 pub struct ScriptingCallback {
     callback: rusty_v8::Global<rusty_v8::Function>,
+}
+#[cfg(not(target_arch = "wasm32"))]
+impl ExecuteFunction for ScriptingCallback
+{
+    fn execute_with_no_args(&self) {
+        todo!()
+    }
+
+    fn execute_with_args(&self, args: Vec<f32>) {
+        todo!()
+    }
+}
+
+
+pub trait ExecuteFunction
+{
+    fn execute_with_no_args(&self);
+
+    // use numbers for now might change to a boxed value
+    fn execute_with_args(&self,args:Vec<f32>);
 }
